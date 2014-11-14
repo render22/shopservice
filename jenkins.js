daeb0beb08dd80d25afe3e8b18c17796fd0e52d8
Viktar <vkantarovich@elinext.com>
2014-11-14 11:14:08 +0200

    Reverting

--- jenkins.js
+++ jenkins.js
@@ -2,17 +2,29 @@ require('./index.js');
 var terminal = require('child_process').spawn('cmd');
 
 terminal.stdout.on('data', function (data) {
-    if (data.toString().trim().length)
+    if (data.toString().trim().length) {
+        if (~data.toString().indexOf('Aborted') || ~data.toString().indexOf('Fatal error')) {
+            console.log(data.toString());
+            process.exit(1);
+        }
         console.log('childTerminal: ' + data);
+
+
+    }
+
 });
 
+terminal.stderr.on('data', function (data) {
+    console.log(data.toString());
+    process.exit(1);
+});
 /*terminal.stderr.on('end', function (data) {
  console.log('childTerminal: ' + data);
  });*/
 
 terminal.on('exit', function (code) {
 
-    process.exit();
+    process.exit(0);
 });
 
 terminal.stdin.write('grunt unittests\n');
